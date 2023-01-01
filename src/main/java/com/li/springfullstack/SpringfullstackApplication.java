e0bdfd8a33db86f41ebd5a4645de6ee36c0920c7
YQ <none>
2023-01-01 13:24:48 -0500

    Start working on Spring full stack project

---
+++ src/main/java/com/li/springfullstack/SpringfullstackApplication.java
@@ -0,0 +1,13 @@
+package com.li.springfullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringfullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringfullstackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/li/springfullstack/SpringfullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.li.springfullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringfullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
