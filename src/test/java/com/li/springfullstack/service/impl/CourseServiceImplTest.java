e53ad2f8cb0b494494e7dd038e6b7d0789e52711
YQ <none>
2023-01-17 17:38:25 -0500

    Start working on Tests

---
+++ src/test/java/com/li/springfullstack/service/impl/CourseServiceImplTest.java
@@ -0,0 +1,40 @@
+package com.li.springfullstack.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CourseServiceImplTest {
+
+    @Test
+    void testLoadCourseById() {
+    }
+
+    @Test
+    void createCourse() {
+    }
+
+    @Test
+    void createOrUpdateCourse() {
+    }
+
+    @Test
+    void findCoursesByCourseName() {
+    }
+
+    @Test
+    void assignStudentToCourse() {
+    }
+
+    @Test
+    void fetchAll() {
+    }
+
+    @Test
+    void fetchCoursesForStudent() {
+    }
+
+    @Test
+    void removeCourse() {
+    }
+}
